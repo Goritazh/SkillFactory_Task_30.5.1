7ed0f1f25ed905832238a1318d88c8c290ceb4a3
Arseny <arseny.uvaroff@gmail.com>
2023-10-11 17:55:13 +0200

    Правки и новый тест

--- tests/test_all_have_dif_name.py
+++ tests/test_all_have_dif_name.py
@@ -1,20 +1,17 @@
 import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from conftest import testing, test_show_my_pets
 
 
 def test_all_have_dif_name(test_show_my_pets):
-    """У всех питомцев разные именаimport pytest"""
+    """Проверяем что у всех питомцев разные имена"""
 
-    element = WebDriverWait(pytest.driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '.table.table-hover tbody tr')))
+    pytest.driver.implicitly_wait(10)
 
     # Сохранение элементов с данными о питомцах в переменную "pet_data"
     pet_data = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
 
-    # создаем список с именами питомцев
+    # Создаем список с именами питомцев
     pets_name = []
     for i in range(len(pet_data)):
         data_pet = pet_data[i].text.replace('\n', '').replace('×', '')
@@ -22,11 +19,9 @@ def test_all_have_dif_name(test_show_my_pets):
         pets_name.append(split_data_pet[0])
 
     # Перебираются имена и, если имя повторяется, к счетчику "r" прибавляется единица
-    # Если r == 0, то повторяющихся имен нет
     r = 0
     for i in range(len(pets_name)):
         if pets_name.count(pets_name[i]) > 1:
             r += 1
+    # Если r == 0, то повторяющихся имен нет
     assert r == 0
-    print(r)
-    print(pets_name)
--- tests/test_all_have_name_age_breed.py
+++ tests/test_all_have_name_age_breed.py
@@ -3,37 +3,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from conftest import testing, test_show_my_pets
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_all_have_name_age_breed(test_show_my_pets):
     """У всех питомцев есть имя, возраст и порода"""
-
-    # Установка явного ожидания
     element = WebDriverWait(pytest.driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, '.table.table-hover tbody tr')))
 
-    # Сохранение элементов с данными о питомцах в переменную "pet_data"
-    pet_data = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
+    # Сохранение элементов с данными о питомцах
+    pets = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
 
-    # Перебираются данные из переменной "pet_data"
-    # Сохраняются имя, возраст и порода, остальное меняется на пустую строку и разделяется по пробелу
-    # Выбираются имена и добавляются в список "pets_name"
-    pets_name = []
-    for i in range(len(pet_data)):
-        data_pet = pet_data[i].text.replace('\n', '').replace('×', '')
+    for i in range(len(pets)):
+        data_pet = pets[i].text.replace('\n', '').replace('×', '')
         split_data_pet = data_pet.split(' ')
-        pets_name.append(split_data_pet[0])
-
-    # Перебираются имена и, если имя повторяется, к счетчику "r" прибавляется единица
-    # Если r == 0, то повторяющихся имен нет
-    r = 0
-    for i in range(len(pets_name)):
-        if pets_name.count(pets_name[i]) > 1:
-            r += 1
-    assert r == 0
-    print(r)
-    print(pets_name)
+        result = len(split_data_pet)
+        assert result == 3
